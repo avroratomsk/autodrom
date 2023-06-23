ce01dc601c75fbcd52c777249bd6c584dd1a6458
sanchyopas <saniagolovanev@gmail.com>
2023-06-24 01:51:39 +0700

    Сделал меню

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,7 +5,6 @@ import { flsModules } from "./modules.js";
 
 
 let btnHideContentMenu = document.querySelector('.header-utp__btn');
-
 if (btnHideContentMenu) {
     btnHideContentMenu.addEventListener('click', openHideContentMenu)
 
@@ -14,3 +13,39 @@ if (btnHideContentMenu) {
         document.querySelector('.hidden-content').classList.toggle('_active')
     }
 }
+
+const btnBurger = document.querySelector('.header-utp__burger');
+if (btnBurger) {
+    btnBurger.addEventListener('click', () => {
+        document.querySelector('.mega-menu').classList.add('_show-menu')
+    })
+}
+
+const btnCloseMenu = document.querySelector('.mega-menu__close');
+if (btnCloseMenu) {
+    btnCloseMenu.addEventListener('click', () => {
+        document.querySelector('.mega-menu').classList.remove('_show-menu')
+    })
+}
+
+let tabTrigger = document.querySelectorAll('.tab__trigger');
+if (tabTrigger.length > 0) {
+    tabTrigger.forEach(trigger => {
+        trigger.addEventListener('click', switchActiveClass)
+    })
+
+    function switchActiveClass() {
+        let dataAttr = this.dataset.id;
+        let tabContent = document.querySelectorAll('.tab');
+
+        if (tabContent.length > 0) {
+            tabContent.forEach(tab => {
+                if (dataAttr === tab.dataset.id) {
+                    tab.classList.add('_active')
+                } else {
+                    tab.classList.remove('_active')
+                }
+            })
+        }
+    }
+} 
