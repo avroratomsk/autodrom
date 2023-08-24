50be6cc1da222c90a3e8d43417877b094c7300d9
sanchyopas <saniagolovanev@gmail.com>
2023-08-24 23:57:21 +0700

    FIX

--- docs/js/app.js
+++ docs/js/app.js
@@ -4077,35 +4077,21 @@
             }));
         }
     }), 0);
-    let btnHideContentMenu = document.querySelector(".header-utp__btn");
-    if (btnHideContentMenu) {
-        btnHideContentMenu.addEventListener("click", openHideContentMenu);
-        function openHideContentMenu() {
-            this.classList.toggle("_active");
-            document.querySelector(".hidden-content").classList.toggle("_active");
-        }
-    }
-    const btnBurger = document.querySelector(".header-utp__burger");
-    if (btnBurger) btnBurger.addEventListener("click", (() => {
-        document.querySelector(".mega-menu").classList.add("_show-menu");
-    }));
-    const btnCloseMenu = document.querySelector(".mega-menu__close");
-    if (btnCloseMenu) btnCloseMenu.addEventListener("click", (() => {
-        document.querySelector(".mega-menu").classList.remove("_show-menu");
-    }));
-    let tabTrigger = document.querySelectorAll(".tab__trigger");
-    if (tabTrigger.length > 0) {
-        tabTrigger.forEach((trigger => {
-            trigger.addEventListener("click", switchActiveClass);
+    let menuItem = document.querySelectorAll(".menu__item-sub");
+    console.log(menuItem);
+    if (menuItem) menuItem.forEach((item => {
+        item.addEventListener("click", (function(e) {
+            this.classList.toggle("_show");
         }));
-        function switchActiveClass() {
-            let dataAttr = this.dataset.id;
-            let tabContent = document.querySelectorAll(".tab");
-            if (tabContent.length > 0) tabContent.forEach((tab => {
-                if (dataAttr === tab.dataset.id) tab.classList.add("_active"); else tab.classList.remove("_active");
-            }));
-        }
-    }
+    }));
+    let buttonsEnroll = document.querySelectorAll(".card-service__btn");
+    if (buttonsEnroll) buttonsEnroll.forEach((item => {
+        item.addEventListener("click", (function(e) {
+            let dataName = this.dataset.name;
+            let inputHideEnrollPopup = document.querySelector(".input__name");
+            if (inputHideEnrollPopup) inputHideEnrollPopup.value = dataName; else console.log("Пусто");
+        }));
+    }));
     window["FLS"] = true;
     isWebp();
     menuInit();
